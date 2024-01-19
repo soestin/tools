d560a279619361e2fb2bb8f31149d9216be6034e
soestin <142896231+soestin@users.noreply.github.com>
2024-01-19 15:49:30 +0100

    Add files via upload

---
+++ dl-tools.ps1
@@ -0,0 +1,11 @@
+$username = "u384419-sub1"
+$password = "eEDjJzJnixri2CuA"  | ConvertTo-SecureString -asPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential($username,$password)
+
+
+Iwr "https://u384419-sub1.your-storagebox.de/HEU_KMS_Activator_41.1.0.exe" -OutFile "HEU_KMS_Activator_41.1.0.exe" -Credential $cred
+Iwr "https://u384419-sub1.your-storagebox.de/PUTTY.EXE" -OutFile "PUTTY.EXE" -Credential $cred
+Iwr "https://u384419-sub1.your-storagebox.de/WiresharkPortable64_4.0.10.paf.exe" -OutFile "WiresharkPortable64_4.0.10.paf.exe" -Credential $cred
+Iwr "https://u384419-sub1.your-storagebox.de/JavaSetup8u381.exe" -OutFile "JavaSetup8u381.exe" -Credential $cred
+Iwr "https://u384419-sub1.your-storagebox.de/rufus-4.2p.exe" -OutFile "rufus-4.2p.exe" -Credential $cred
+Iwr "https://u384419-sub1.your-storagebox.de/WinSCP-6.1.2-Setup.exe" -OutFile "WinSCP-6.1.2-Setup.exe" -Credential $cred
