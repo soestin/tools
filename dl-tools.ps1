c34f74769d48290a7ba20f7bdbb36eb07e25b548
soestin <142896231+soestin@users.noreply.github.com>
2024-01-20 14:49:37 +0100

    Update dl-tools.ps1

--- dl-tools.ps1
+++ dl-tools.ps1
@@ -6,6 +6,6 @@ $cred = New-Object System.Management.Automation.PSCredential($username,$password
 Iwr "https://u384419-sub1.your-storagebox.de/HEU_KMS_Activator_41.1.0.exe" -OutFile "HEU_KMS_Activator_41.1.0.exe" -Credential $cred
 Iwr "https://u384419-sub1.your-storagebox.de/PUTTY.EXE" -OutFile "PUTTY.EXE" -Credential $cred
 Iwr "https://u384419-sub1.your-storagebox.de/WiresharkPortable64_4.0.10.paf.exe" -OutFile "WiresharkPortable64_4.0.10.paf.exe" -Credential $cred
-Iwr "https://u384419-sub1.your-storagebox.de/JavaSetup8u381.exe" -OutFile "JavaSetup8u381.exe" -Credential $cred
-Iwr "https://u384419-sub1.your-storagebox.de/rufus-4.2p.exe" -OutFile "rufus-4.2p.exe" -Credential $cred
+Iwr "https://u384419-sub1.your-storagebox.de/jdk-8.exe" -OutFile "Java-8.exe" -Credential $cred
+Iwr "https://u384419-sub1.your-storagebox.de/rufus.exe" -OutFile "rufus.exe" -Credential $cred
 Iwr "https://u384419-sub1.your-storagebox.de/WinSCP-6.1.2-Setup.exe" -OutFile "WinSCP-6.1.2-Setup.exe" -Credential $cred
